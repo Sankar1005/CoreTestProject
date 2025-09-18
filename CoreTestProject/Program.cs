5ca7d30c78800b35e1ce191d9fa4acc43fbaa366
Sankar1005 <hsankar243@gmail.com>
2025-09-18 16:06:59 +0530

    Add project files.

---
+++ CoreTestProject/Program.cs
@@ -0,0 +1,20 @@
+namespace CoreTestProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+            //app.MapControllerRoute(
+            //    name:"default",
+            //    pattern:"{controller=Home}/{action=Index}/{id?}");
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
